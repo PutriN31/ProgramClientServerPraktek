285400a7829af9955f057a1217957fb7024c746d
PutriN31 <113873464+PutriN31@users.noreply.github.com>
2022-09-27 12:40:52 +0700

    Add files via upload
    JDK - 17.0.4.1
    Packaging JAR
    Spring Boot 2.7.4

---
+++ user-service/user-service/src/main/java/com/putri/user/service/dto/User.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.putri.user.service.dto;
+
+/**
+ *
+ * @author user
+ */
+public class User {
+    private String id;
+    private  String name;
+    private String Address;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String Address) {
+        this.Address = Address;
+    }
+    
+    
+}
